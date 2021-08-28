8fad645b42b9f0cc2ff35e6f8f8b3045338c48f6
GitHub Action <action@github.com>
2021-08-28 12:20:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/aymanosman/intellijtest2/services/MyProjectService.kt
+++ src/main/kotlin/com/github/aymanosman/intellijtest2/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.aymanosman.intellijtest2.services
 
+import com.github.aymanosman.intellijtest2.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
